78abab2c3e9676e5f4bb90c7caa57a3dc86c40b1
Bartosz <bartek.pawlowski00@gmail.com>
2023-01-25 07:35:02 +0100

    resolved

--- main.cpp
+++ main.cpp
@@ -4,57 +4,50 @@
 #include "Eigen/Dense"
 #include <vector>
 #include <string>
-//#include "matplotlibcpp.h"
-
-#define N 10
-#define BETA  1.0
-#define GAMMA  1.0
-#define U1 exp(-1)
+#include <cstring>
+#include "matplotlibcpp.h"
 
 using namespace std;
 using namespace Eigen;
-//namespace plt = matplotlibcpp;
+namespace plt = matplotlibcpp;
 
-function<double(double)> multiplyTwoFunctions(function<double(double)> f1, function<double(double)> f2) {
+
+function<double(double)> multiply_two_functions(function<double(double)> f1, function<double(double)> f2) {
     return [f1, f2](double x) { return f1(x) * f2(x); };
 }
 
-function<double(double)> multiplyThreeFunctions(function<double(double)> f1, function<double(double)> f2, function<double(double)> f3) {
-    return [f1, f2, f3](double x) { return f1(x) * f2(x) * f3(x); };
+
+double max(double a, double b, double c) {
+    double x = max(a, b);
+    double y = max(b, c);
+    return max(x, y);
 }
 
-double function_a(double x) {
-    return 0.0;
+
+double x_i(int i, int n) {
+    return (double) (2*i)/n;
 }
 
-double function_b(double x) {
-    return 1.0;
-}
-
-double function_c(double x) {
-    return 2.0;
-}
-
-double function_f(double x) {
-    return exp(-x);
-}
 
 function<double(double)> e(int i, int n) {
     return [i, n](double x) {
-        if (1.0 - abs(n * (x - i / n)) >= 0) {
-            return 1.0 - abs(n * (x - i / n));
+        if (x > x_i(i - 1, n) && x <= x_i(i, n)) {
+            return (double) n/2*x - i + 1;
+        } else if (x > x_i(i, n) && x < x_i(i + 1, n)) {
+            return (double) -n/2*x + i + 1;
         } else {
             return 0.0;
         }
     };
 }
 
+
 function<double(double)> e_der(int i, int n) {
     return [i, n](double x) {
-        if (((i - 1) / n <= x) && (x < i / n)) {
-            return (double) n;
-        } else if (((i + 1) / n > x) && (x >= i / n)){
-            return (double) -n;
+        if (x > x_i(i - 1, n) && x <= x_i(i, n)) {
+            return (double) n/2;
+        } else if (x > x_i(i, n) && x < x_i(i + 1, n)) {
+            return (double) -n/2;
         } else {
             return 0.0;
         }
@@ -71,103 +64,125 @@ double integration(function<double(double)> f, double a, double b) {
 }
 
 
-double B(function<double(double)> u_der, function<double(double)> v_der, function<double(double)> u, function<double(double)> v, double start, double end) {
-    function<double(double)> f1 = multiplyThreeFunctions(function_a, u_der, v_der);
-    function<double(double)> f2 = multiplyThreeFunctions(function_b, u_der, v);
-    function<double(double)> f3 = multiplyThreeFunctions(function_c, u, v);
-    return -u(0)*v(0)*BETA - integration(f1, start, end) + integration(f2, start, end) + integration(f3, start, end);
+double B(double x, int i, int j, int n) {
+    function<double(double)> f = multiply_two_functions(e_der(i, n), e_der(j, n));
+    double start = max(0.0, x_i(i - 1, n), x_i(j - 1, n));
+    double end = min(x_i(i + 1, n), x_i(j + 1, n));
+    return e(i, n)(x) * e(j, n)(x) - integration(f, start, end);
 }
 
 
-double L(function<double(double)> v, double start, double end) {
-    function<double(double)> f = multiplyTwoFunctions(v, function_f);
-    return integration(f, start, end) - GAMMA*v(0);
+double L(double x, int i, int n) {
+    return 20 * e(i, n) (x);
 }
 
 
-Eigen::Matrix<float, N, N> initialize_matrix() {
-    Matrix<float, N, N> matrix;
+Eigen::Matrix<float, Dynamic, Dynamic> initialize_main_matrix(int n) {
+    Eigen::Matrix<float, Dynamic, Dynamic> matrix;
+    matrix.resize(n, n);
     matrix.setZero();
-    double start, end;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (abs(i - j) > 0) {
-                matrix(i, j) = 0.0;
-                continue;
-            }
-
-            if (abs(i - j) == 1) {
-                start = max(0.0, (double) min(i, j)/N);
-                end = min(1.0, (double) max(i, j)/N);
-            } else {
-                start = max(0.0, (double) (i - 1)/N);
-                end = min(1.0, (double) (i + 1)/N);
-            }
-            matrix(i, j) = B(e_der(j, N), e_der(i, N), e(j, N), e(i, N), start, end);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix(i, j) = B(0, i, j, n);
         }
     }
     return matrix;
 }
 
 
-Eigen::Matrix<float, N, 1> initialize_vector() {
-    Matrix<float, N, 1> vector;
-    for (int i = 0; i < N; i++) {
-        double start = max(0.0, (double) i/N - 1.0/N);
-        double end = min(1.0, (double) i/N + 1.0/N);
-        vector(i, 0) = L(e(i, N), start, end);
+Eigen::Matrix<float, Dynamic, Dynamic> initialize_constant_matrix(int n) {
+    Eigen::Matrix<float, Dynamic, Dynamic> matrix;
+    matrix.resize(n, 1);
+    for (int i = 0; i < n; i++) {
+        matrix(i, 0) = L(0, i, n);
     }
-    return vector;
+    return matrix;
 }
 
-Eigen::Matrix<float, N, 1> solve_linear_matrix_equation(Eigen::Matrix<float, N, N> matrix, Eigen::Matrix<float, N, 1> vector) {
-    Eigen::ColPivHouseholderQR<Eigen::Matrix<float, N, N>> dec(matrix);
-    Eigen::Matrix<float, N, 1> solution = dec.solve(vector);
+
+Eigen::Matrix<float, Dynamic, Dynamic> solve_linear_matrix_equation(Eigen::Matrix<float, Dynamic, Dynamic> main_matrix, Eigen::Matrix<float, Dynamic, Dynamic> constant_matrix, int n) {
+    Eigen::ColPivHouseholderQR<Eigen::Matrix<float, Dynamic, Dynamic>> dec(main_matrix);
+    Eigen::Matrix<float, Dynamic, Dynamic> solution;
+    solution.resize(n, 1);
+    solution = dec.solve(constant_matrix);
     return solution;
 }
 
-vector<double> initialize_x_values() {
+
+vector<double> initialize_x_values(int n) {
     vector<double> result;
-    for (int i = 0; i <= 1000; i++) {
-        result.push_back((double) i / 1000);
+    double value = 0.0;
+    double step = 2.0/n;
+
+    for (int i = 0; i < n; i++) {
+        value += step;
+        result.push_back(value);
     }
     return result;
 }
 
-vector<double> initialize_y_values(Eigen::Matrix<float, N, 1> matrix) {
+
+vector<double> initialize_y_values(Eigen::Matrix<float, Dynamic, 1> matrix, vector<double> x_values, int n) {
     vector<double> result;
-    for (int x = 0; x <= 1000; x++) {
+    matrix.resize(n, 1);
+    for (int i = 0; i < n; i++) {
         double sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += matrix(i, 0) * e(i, N) ((double) x / 1000);
+        for (int j = 0; j < n; j++) {
+            sum += matrix(j, 0) * (e(j, n) (x_values[i]));
         }
-        sum += x * U1 / 1000;
         result.push_back(sum);
     }
     return result;
 }
 
-void print_calculated_value(vector<double> x, vector<double> y) {
+
+void print_calculated_value(vector<double> x, vector<double> y, int n) {
     cout << "Equation solved. Calculated values (x, y): " << endl;
-    for (int i = 0; i <= 1000; i++) {
+    for (int i = 0; i < n; i++) {
         cout << "(" + to_string(x[i]) + ", " + to_string(y[i]) + ") ";
     }
 }
 
-void draw_plot(vector<double> x, vector<double> y) {
 
+void draw_plot(vector<double> x, vector<double> y) {
+    plt::figure();
+    plt::plot(x, y);
+    plt::title("Rozwiązanie równania transportu ciepła.");
+    plt::show();
 }
 
 
-int main() {
-    Eigen::Matrix<float, N, N> matrix = initialize_matrix();
-    Eigen::Matrix<float, N, 1> vectorLELE = initialize_vector();
-    Eigen::Matrix<float, N, 1> solution = solve_linear_matrix_equation(matrix, vectorLELE);
+void check_arguments(int argc, char* argv[]) {
+    if (argc != 2) {
+        cout << "invalid number of arguments!" << endl;
+        exit(1);
+    }
+    const char *input_argument = argv[1];
+    for (int i = 0; i < strlen(input_argument); i++) {
+        if(!isdigit(input_argument[i])) {
+            cout<<"The input argument must be an integer!" << endl;
+            exit(2);
+        }
+    }
+    if(atoi(argv[1]) < 5 || atoi(argv[1]) > 1000) {
+        cout << "The input argument must be in the range of 5 to 1000!" << endl;
+        exit(3);
+    };
+}
 
-    vector<double> x_values = initialize_x_values();
-    vector<double> y_values = initialize_y_values(solution);
 
-    print_calculated_value(x_values, y_values);
+int main(int argc, char* argv[]) {
+    check_arguments(argc, argv);
+    int n = atoi(argv[1]);
+
+    Eigen::Matrix<float, Dynamic, Dynamic> main_matrix = initialize_main_matrix(n);
+    Eigen::Matrix<float, Dynamic, Dynamic> constant_matrix = initialize_constant_matrix(n);
+    Eigen::Matrix<float, Dynamic, Dynamic> solution = solve_linear_matrix_equation(main_matrix, constant_matrix, n);
+
+    vector<double> x_values = initialize_x_values(n);
+    vector<double> y_values = initialize_y_values(solution, x_values, n);
+
+    print_calculated_value(x_values, y_values, n);
     draw_plot(x_values, y_values);
 
     return 0;
